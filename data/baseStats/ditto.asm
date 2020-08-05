10f4198abebbea67d10adcefca26bcb834f5bfcc
ludi1893 <64689645+ludi1893@users.noreply.github.com>
2020-08-05 14:15:12 +0200

    Update ditto.asm

--- data/baseStats/ditto.asm
+++ data/baseStats/ditto.asm
@@ -2,7 +2,7 @@ db DEX_DITTO ; pokedex id
 db 58 ; base hp
 db 58 ; base attack
 db 58 ; base defense
-db 58 ; base speed
+db 88 ; base speed
 db 58 ; base special
 db NORMAL ; species type 1
 db NORMAL ; species type 2
