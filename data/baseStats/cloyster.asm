948551bbcaa0d4e0bd6b4e9dabbd2f64f90ed304
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2020-11-05 20:37:48 +0100

    Update cloyster.asm

--- data/baseStats/cloyster.asm
+++ data/baseStats/cloyster.asm
@@ -12,10 +12,10 @@ INCBIN "pic/swmon/cloyster.pic",0,1 ; 77, sprite dimensions
 dw CloysterPicFront
 dw CloysterPicBack
 ; attacks known at lvl 0
-db WITHDRAW
-db SUPERSONIC
+db EXPLOSION
 db CLAMP
-db AURORA_BEAM
+db SURF
+db ICE_BEAM
 db 5 ; growth rate
 ; learnset
 	tmlearn 6
