b33160a0c9e4e752e95b74cc251f55455f1975a5
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2020-11-17 15:00:51 +0100

    Update viridianforest.asm

--- data/wildPokemon/viridianforest.asm
+++ data/wildPokemon/viridianforest.asm
@@ -1,25 +1,25 @@
 ForestMons:
 	db $08
 	IF DEF(_RED)
-		db 4,WEEDLE
-		db 5,KAKUNA
-		db 3,WEEDLE
-		db 5,WEEDLE
-		db 4,KAKUNA
-		db 6,KAKUNA
-		db 4,METAPOD
-		db 3,CATERPIE
+		db 6,WEEDLE
+		db 7,KAKUNA
+		db 6,WEEDLE
+		db 6,WEEDLE
+		db 7,KAKUNA
+		db 7,KAKUNA
+		db 7,METAPOD
+		db 10,BUTTERFREE
 	ENDC
 	IF DEF(_BLUE)
-		db 4,CATERPIE
-		db 5,METAPOD
-		db 3,CATERPIE
-		db 5,CATERPIE
-		db 4,METAPOD
-		db 6,METAPOD
-		db 4,KAKUNA
-		db 3,WEEDLE
+		db 6,CATERPIE
+		db 7,METAPOD
+		db 6,CATERPIE
+		db 6,CATERPIE
+		db 7,METAPOD
+		db 7,METAPOD
+		db 7,KAKUNA
+		db 10,BEEDRILL
 	ENDC
-	db 3,PIKACHU
 	db 5,PIKACHU
+	db 9,PIKACHU
 	db $00
