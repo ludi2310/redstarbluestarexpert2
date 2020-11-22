b7af4543b5718930fd36c9de5ce4ecea7752423c
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2020-11-22 12:46:06 +0100

    Update dodrio.asm

--- data/baseStats/dodrio.asm
+++ data/baseStats/dodrio.asm
@@ -12,9 +12,9 @@ INCBIN "pic/swmon/dodrio.pic",0,1 ; 77, sprite dimensions
 dw DodrioPicFront
 dw DodrioPicBack
 ; attacks known at lvl 0
-db PECK
-db GROWL
-db FURY_ATTACK
+db SWORDS_DANCE
+db TRI_ATTACK
+db DRILL_PECK
 db 0
 db 0 ; growth rate
 ; learnset
