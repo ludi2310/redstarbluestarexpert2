82a58d1d7ce065865df2e87b39b57bf419c4bc7b
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 13:05:09 +0100

    Update pinsir.asm

--- data/baseStats/pinsir.asm
+++ data/baseStats/pinsir.asm
@@ -1,11 +1,11 @@
 db DEX_PINSIR ; pokedex id
-db 65 ; base hp
+db 95 ; base hp
 db 125 ; base attack
 db 100 ; base defense
 db 85 ; base speed
 db 55 ; base special
 db BUG ; species type 1
-db BUG ; species type 2
+db GROUND ; species type 2
 db 45 ; catch rate
 db 200 ; base exp yield
 INCBIN "pic/swmon/pinsir.pic",0,1 ; 77, sprite dimensions
