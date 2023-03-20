52826adc81395be2da34d57296d41e6e8dd909cb
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 12:10:12 +0100

    Update charizard.asm

--- data/baseStats/charizard.asm
+++ data/baseStats/charizard.asm
@@ -2,8 +2,8 @@ db DEX_CHARIZARD ; pokedex id
 db 78 ; base hp
 db 84 ; base attack
 db 78 ; base defense
-db 100 ; base speed
-db 85 ; base special
+db 110 ; base speed
+db 105 ; base special
 db FIRE ; species type 1
 db FLYING ; species type 2
 db 45 ; catch rate
