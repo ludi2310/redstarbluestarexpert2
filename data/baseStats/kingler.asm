6fade392dba72481d9d1d9694ae3c537e3f1d20f
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 12:57:17 +0100

    Update kingler.asm

--- data/baseStats/kingler.asm
+++ data/baseStats/kingler.asm
@@ -1,20 +1,20 @@
 db DEX_KINGLER ; pokedex id
-db 55 ; base hp
+db 65 ; base hp
 db 130 ; base attack
-db 115 ; base defense
+db 125 ; base defense
 db 75 ; base speed
-db 50 ; base special
+db 90 ; base special
 db WATER ; species type 1
-db WATER ; species type 2
+db BUG ; species type 2
 db 60 ; catch rate
 db 206 ; base exp yield
 INCBIN "pic/swmon/kingler.pic",0,1 ; 77, sprite dimensions
 dw KinglerPicFront
 dw KinglerPicBack
 ; attacks known at lvl 0
-db BUBBLE
-db LEER
-db VICEGRIP
+db SURF
+db SWORDS_DANCE
+db CUT
 db 0
 db 0 ; growth rate
 ; learnset
