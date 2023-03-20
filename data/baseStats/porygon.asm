6e7a346963fae02a24669962dce55d2631d39694
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 12:40:19 +0100

    Update porygon.asm

--- data/baseStats/porygon.asm
+++ data/baseStats/porygon.asm
@@ -1,20 +1,20 @@
 db DEX_PORYGON ; pokedex id
-db 65 ; base hp
-db 60 ; base attack
-db 70 ; base defense
+db 95 ; base hp
+db 70 ; base attack
+db 100 ; base defense
 db 40 ; base speed
-db 75 ; base special
-db NORMAL ; species type 1
-db NORMAL ; species type 2
+db 125 ; base special
+db ELECTRIC ; species type 1
+db PSYCHIC ; species type 2
 db 45 ; catch rate
 db 130 ; base exp yield
 INCBIN "pic/swmon/porygon.pic",0,1 ; 66, sprite dimensions
 dw PorygonPicFront
 dw PorygonPicBack
 ; attacks known at lvl 0
-db TACKLE
+db CONFUSION
 db SHARPEN
-db CONVERSION
+db RECOVER
 db 0
 db 0 ; growth rate
 ; learnset
