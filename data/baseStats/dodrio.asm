4ecd0b8ecd3b7b717dda67552b8089803eb9b4d6
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 12:51:11 +0100

    Update dodrio.asm

--- data/baseStats/dodrio.asm
+++ data/baseStats/dodrio.asm
@@ -2,10 +2,10 @@ db DEX_DODRIO ; pokedex id
 db 60 ; base hp
 db 110 ; base attack
 db 70 ; base defense
-db 100 ; base speed
+db 140 ; base speed
 db 60 ; base special
-db NORMAL ; species type 1
-db FLYING ; species type 2
+db FLYING ; species type 1
+db GROUND ; species type 2
 db 45 ; catch rate
 db 158 ; base exp yield
 INCBIN "pic/swmon/dodrio.pic",0,1 ; 77, sprite dimensions
