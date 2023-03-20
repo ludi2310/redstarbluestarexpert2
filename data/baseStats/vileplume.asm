7f082bedd02d61cc2b722ad6e6237ce07e3c77da
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 12:21:02 +0100

    Update vileplume.asm

--- data/baseStats/vileplume.asm
+++ data/baseStats/vileplume.asm
@@ -1,11 +1,11 @@
 db DEX_VILEPLUME ; pokedex id
-db 75 ; base hp
-db 80 ; base attack
+db 95 ; base hp
+db 90 ; base attack
 db 85 ; base defense
 db 50 ; base speed
-db 100 ; base special
+db 110 ; base special
 db GRASS ; species type 1
-db POISON ; species type 2
+db GHOST ; species type 2
 db 45 ; catch rate
 db 184 ; base exp yield
 INCBIN "pic/swmon/vileplume.pic",0,1 ; 77, sprite dimensions
