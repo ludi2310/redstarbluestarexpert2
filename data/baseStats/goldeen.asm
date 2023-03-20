409743a693f438a63ca390d72a2d45754c296210
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2023-03-20 13:02:26 +0100

    Update goldeen.asm

--- data/baseStats/goldeen.asm
+++ data/baseStats/goldeen.asm
@@ -1,18 +1,18 @@
 db DEX_GOLDEEN ; pokedex id
 db 45 ; base hp
-db 67 ; base attack
+db 77 ; base attack
 db 60 ; base defense
 db 63 ; base speed
 db 50 ; base special
 db WATER ; species type 1
-db WATER ; species type 2
+db NORMAL ; species type 2
 db 225 ; catch rate
 db 111 ; base exp yield
 INCBIN "pic/swmon/goldeen.pic",0,1 ; 66, sprite dimensions
 dw GoldeenPicFront
 dw GoldeenPicBack
 ; attacks known at lvl 0
-db PECK
+db HORN_ATTACK
 db TAIL_WHIP
 db 0
 db 0
